9611797804e82b0754cf6ae04c4d62d6ee5c1630
shannequin <shannequin@gmail.com>
2026-03-21 13:20:41 -0500

    Create hop_team_nashville_mv

--- hop_team_exploratory_analysis_shannon.sql
+++ hop_team_exploratory_analysis_shannon.sql
@@ -37,8 +37,23 @@ ORDER BY zip;
 -- WHERE table_name = 'nppes'
 -- 	AND column_name LIKE 'healthcare_primary_taxonomy_switch%'
 
--- Create a materialized view for NPPES in TN with primary taxonomy column
--- CREATE MATERIALIZED VIEW nppes_tn_primary_taxonomy_mv AS
+--------------------------------------------------
+-- ZIP CBSA
+--------------------------------------------------
+
+-- Create a materialized view for Nashville CBSA (34980)
+-- CREATE MATERIALIZED VIEW zip_cbsa_nashville_mv AS
+SELECT *
+FROM zip_cbsa
+WHERE cbsa = '34980'; -- 157 rows
+
+
+--------------------------------------------------
+-- NPPES
+--------------------------------------------------
+
+-- Create a materialized view for NPPES in Nashville with primary taxonomy column and formatted zip code
+-- CREATE MATERIALIZED VIEW nppes_nashville_mv AS
 SELECT
 	*,
 	CASE
@@ -59,10 +74,9 @@ SELECT
 		WHEN healthcare_primary_taxonomy_switch_15 IN ('Y', 'X') THEN healthcare_taxonomy_code_15
 		ELSE NULL
 	END AS primary_taxonomy_code
-FROM nppes
-WHERE address_state_name IN ('TENNESSEE', 'TN')
-ORDER BY npi;
--- 138156 rows
+FROM nppes n
+	INNER JOIN zip_cbsa_nashville_mv z ON LEFT(n.address_postal_code::TEXT, 5) = z.zip::TEXT
+ORDER BY npi; -- 50824 rows
 
 --------------------------------------------------
 -- REFERRING PROVIDERS
@@ -71,29 +85,24 @@ ORDER BY npi;
 -- nucc.classification=<pcp>
 -- nppes.type=1 (people)
 
--- Create a materialized view for NUCC PCPs
--- CREATE MATERIALIZED VIEW nucc_pcp_mv AS
+-- Create a materialized view for NPPES PCPs in Nashville
+-- CREATE MATERIALIZED VIEW nppes_nashville_pcp_mv AS
+WITH nucc_pcp AS (
+	SELECT *
+	FROM nucc
+	WHERE classification ILIKE ANY(
+		ARRAY[
+			'family medicine',
+			'internal medicine',
+			'pediatrics',
+			'general practice'
+		]
+	)
+) -- 68 ROWS
 SELECT *
-FROM nucc
-WHERE classification ILIKE ANY(
-	ARRAY[
-		'family medicine',
-		'internal medicine',
-		'pediatrics',
-		'general practice'
-	]
-)
-ORDER BY code;
--- 68 ROWS
-
--- Create a materialized view for NPPES PCPs in TN with primary taxonomy column
--- (UPDATE TO USE CBSA WHEN READY)
--- CREATE MATERIALIZED VIEW nppes_tn_pcp_mv AS
-SELECT *
-FROM nppes_tn_primary_taxonomy_mv n
-	INNER JOIN nucc_pcp_mv p ON n.primary_taxonomy_code = p.code
-WHERE entity_type_code = 1;	
--- 13157 rows
+FROM nppes_nashville_mv n
+	INNER JOIN nucc_pcp p ON n.primary_taxonomy_code = p.code
+WHERE entity_type_code = 1;	-- 3621 rows
 
 
 --------------------------------------------------
@@ -103,30 +112,44 @@ WHERE entity_type_code = 1;
 -- nucc.grouping=hospital
 -- nppes.entity_type=2 (orgs)
 
--- Create a materialized view for NUCC Hospitals
--- CREATE MATERIALIZED VIEW nucc_hospitals_mv AS
+-- Create a materialized view for NPPES hospitals in Nashville
+-- CREATE MATERIALIZED VIEW nppes_nashville_hospital_mv AS
+WITH nucc_hospital AS (
+	SELECT *
+	FROM nucc
+	WHERE grouping ILIKE '%hospital%'
+) -- 23 ROWS
 SELECT *
-FROM nucc
-WHERE grouping ILIKE '%hospital%'
-ORDER BY code;
--- 23 ROWS
-
--- Create a materialized view for NPPES hospitals in TN with primary taxonomy column
--- (UPDATE TO USE CBSA WHEN READY)
--- CREATE MATERIALIZED VIEW nppes_tn_hospitals_mv AS
-SELECT *
-FROM nppes_tn_primary_taxonomy_mv n
-	INNER JOIN nucc_hospitals_mv h ON n.primary_taxonomy_code = h.code
-WHERE entity_type_code = 2;
--- 635 rows
+FROM nppes_nashville_mv n
+	INNER JOIN nucc_hospital h ON n.primary_taxonomy_code = h.code
+WHERE entity_type_code = 2; -- 204 rows
 
 
 --------------------------------------------------
 -- HOP TEAM
 --------------------------------------------------
 
--- CREATE MATERIALIZED VIEW hop_team_tn_pcp_hospitals_mv AS
-SELECT h.*
+-- Create a materialized view for hop_team in Nashville with PCP referals and receiving hosptals
+-- Filter significant referral relationships to avoid incidental or low-volume referrals
+-- CREATE MATERIALIZED VIEW hop_team_nashville_mv AS
+SELECT
+	h.from_npi AS referring_pcp_npi,
+	np.first_name || ' ' || np.last_name AS pcp,
+	np.grouping AS pcp_grouping,
+	np.classification AS pcp_classification,
+	np.specialization AS pcp_specialization,
+	h.to_npi AS receiving_hospital_npi,
+	nh.organization_name AS hospital,
+	nh.grouping AS hospital_grouping,
+	nh.classification AS hospital_classification,
+	nh.specialization AS hospital_specialization,
+	h.patient_count,
+	h.transaction_count,
+	h.average_day_wait,
+	h.std_day_wait
 FROM hop_team h
-	INNER JOIN nppes_tn_pcp_mv np ON h.from_npi = np.npi
-	INNER JOIN nppes_tn_hospitals_mv nh ON h.to_npi = nh.npi; -- 34350 rows
+	INNER JOIN nppes_nashville_pcp_mv np ON h.from_npi = np.npi
+	INNER JOIN nppes_nashville_hospital_mv nh ON h.to_npi = nh.npi
+WHERE transaction_count >= 50
+	AND average_day_wait < 50
+ORDER BY hospital, pcp; -- 2785 rows
