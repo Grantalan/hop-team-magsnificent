989b4c02259e415c68eb15f28ea4cf037033fbce
Grant Alan <Grantmodels.ai@gmail.com>
2026-03-31 20:50:05 -0500

    update treemap in analysis tab

--- hop_team_app/Introduction.py
+++ hop_team_app/Introduction.py
@@ -22,7 +22,7 @@ with goals_col1:
     '''
 
 with goals_col2:
-    st.image('images/goals.png', width=200)
+    st.image('../images/goals.png', width=200)
 
 st.space('small')
 
@@ -33,7 +33,7 @@ st.header('Dataset', divider='rainbow', text_alignment='left')
 data_col1, data_col2 = st.columns([1, 2], vertical_alignment='center')
 
 with data_col1:
-    st.image('images/dataset.png', width=250)
+    st.image('../images/dataset.png', width=250)
 
 with data_col2:
     '''
@@ -58,7 +58,7 @@ with postgres_col1:
     '''
 
 with postgres_col2:
-    st.image('images/postgresql.png', width=75)
+    st.image('../images/postgresql.png', width=75)
 
 neo_col1, neo_col2 = st.columns([2, 1], vertical_alignment='center')
 
@@ -66,7 +66,7 @@ with neo_col1:
     "* **Neo4j:** The World's Leading Graph Intelligence Platform"
 
 with neo_col2:
-    st.image('images/neo4j.png', width=75)
+    st.image('../images/neo4j.png', width=75)
 
 streamlit_col1, streamlit_col2 = st.columns([2, 1], vertical_alignment='center')
 
@@ -74,7 +74,7 @@ with streamlit_col1:
     '* **Streamlit:** an open-source app framework'
 
 with streamlit_col2:
-    st.image('images/streamlit.png', width=75)
+    st.image('../images/streamlit.png', width=75)
 
 st.space('large')
 
@@ -87,16 +87,16 @@ nss_col1, nss_col2, nss_col3, nss_col4, nss_col5, nss_col6 = st.columns([2, 1, 1
 
 with nss_col2:
     'Grant Alan'
-    st.image('images/grant.png')
+    st.image('../images/grant.png')
 
 with nss_col3:
     'Abigail Ezell'
-    st.image('images/abigail.png')
+    st.image('../images/abigail.png')
 
 with nss_col4:
     'Shannon Lee'
-    st.image('images/shannon.png')
+    st.image('../images/shannon.png')
 
 with nss_col5:
     'Micheal Major'
-    st.image('images/micheal.png')
+    st.image('../images/micheal.png')
